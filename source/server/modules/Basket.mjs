32ac367fad526b720502a36005c4168418f894b5
kir3i <kirei7081@gmail.com>
2020-11-15 23:57:31 +0900

    Basket add_order 로직, Customer pay 로직 수정

--- source/server/modules/Basket.mjs
+++ source/server/modules/Basket.mjs
@@ -1,10 +1,19 @@
 "use strict"
 
+import Order from "./Order.mjs";
+
 export default class Basket {
     #_order_list = [];
 
-    add_order(order) {
-        this.#_order_list.push(order);
+    add_order(order_info) {
+        let new_order = new Order(
+            order_info[menu], 
+            order_info[style], 
+            order_info[foods_to_remove], 
+            order_info[amount], 
+            order_info[additional_info]
+        );
+        this.#_order_list.push(new_order);
     }
 
     get order_list() {
--- source/server/modules/Customer.mjs
+++ source/server/modules/Customer.mjs
@@ -1,6 +1,7 @@
 "use strict";
 
 import Basket from './Basket.mjs';
+import Order_list from './Order_list.mjs';
 
 export default class Customer {
     #_id;
@@ -33,6 +34,12 @@ export default class Customer {
     pay(pay_info) {
         // TODO: 최근 주문 내역에 현재 주문 리스트 추가 (DB)
         console.log('paying...');
+        
+        // 결제가 완료되었을 시 Order_list로 넘김
+        for (o of this.#_basket.order_list) {
+            // Order_list.add_order(o);
+            console.log(x);
+        }
     }
 
 }
