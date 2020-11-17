4f00769a96d9d51d2591c4db58c49b0e871c3605
kir3i <kirei7081@gmail.com>
2020-11-17 17:35:45 +0900

    member 데이터 저장하도록 수정

--- source/server/modules/Customer.mjs
+++ source/server/modules/Customer.mjs
@@ -1,5 +1,6 @@
 "use strict";
 
+import fs from 'fs'
 import Basket from './Basket.mjs';
 import Order_list from './Order_list.mjs';
 
@@ -34,12 +35,29 @@ export default class Customer {
     pay(pay_info) {
         // TODO: 최근 주문 내역에 현재 주문 리스트 추가 (DB)
         console.log('paying...');
-        
+        // 결제 시스템에게 요청
+        // const ok = pay(pay_info);
+
         // 결제가 완료되었을 시 Order_list로 넘김
-        for (o of this.#_basket.order_list) {
-            // Order_list.add_order(o);
-            console.log(x);
-        }
+        //if(ok) {}
+            for (const o of this.#_basket.order_list) {
+                Order_list.add_order(o);
+            }
+
+            // DB에 입력
+            const user_str = fs.readFileSync(`./data/users/${this.#_id}.json`, 'utf8');
+            const user = JSON.parse(user_str);
+            user.info.call_cnt += 1;
+            user.info.recent_ordered_menu.push(...this.#_basket.order_list);
+
+            //console.log(user);
+
+            const userJson = JSON.stringify(user);
+            fs.writeFileSync(`./data/users/${this.#_id}.json`, userJson, 'utf8');
+            
+            return true;
+        // 결제가 실패하면
+        // else{ return false; }   //에러 리턴
     }
 
 }
--- source/server/modules/Member_management.mjs
+++ source/server/modules/Member_management.mjs
@@ -1,23 +1,53 @@
 "use strict"
 
+import fs from 'fs';
 import Customer from "./Customer.mjs";
 
 export default class Member_management {
     static #_activated_member_list = {};
 
     static signup(id, pw, info) {
-        // TODO: DB에 추가 로직
+        const user = {
+            'id': id,
+            'pw': pw,
+            'info': info
+        };
+        
+        const userJson = JSON.stringify(user);
+
+        try {
+            fs.writeFileSync(`./data/users/${id}.json`, userJson, 'utf8');
+        } catch(e) {
+            console.log(e);
+        }
+
         this.#_activated_member_list[id] = new Customer(id, false, null);
     }
 
     static login(id, pw) {
-        // TODO: 타당성 검사 로직
-        if(true) { // 고객이면
-            this.#_activated_member_list[id] = new Customer(id, false, []);
-            // 단골고객 여부, 최근 주문 내역 fetch 해와서 넘겨줌
-        }
+        //console.log(`file load from ./data/users/${id}.json`);
         
-        return id;  // 직원이면 따로 객체 생성 없이 return
+        try {
+            const user_str = fs.readFileSync(`./data/users/${id}.json`, 'utf8');
+            const user = JSON.parse(user_str);
+            
+            if(user['pw'] !== pw) {
+                //로그인 실패
+                console.log('Login Failed:: Wrong PW');
+                return null;
+            }
+            else if(user.info.is_customer) {
+                // Customer 객체 생성
+                this.#_activated_member_list[id] = new Customer(
+                    id, user.info.is_regular, user.info.recent_ordered_menu);
+            }
+        } catch(e) {
+            // 로그인 실패
+            console.log(e);
+            return null;
+        }
+
+        return id;
     }
 
     static deactivate(id) {
@@ -28,3 +58,33 @@ export default class Member_management {
         return this.#_activated_member_list;
     }
 }
+
+/*
+Member_management.signup(
+        'wak',
+        '0724', 
+        {
+            'is_customer': true, 
+            'recent_ordered_menu': ['메뉴1', '메뉴2'], 
+            'is_regular': false,
+            'call_cnt': 0
+        });
+console.log(Member_management.activated_member_list);
+Member_management.deactivate('wak');
+console.log(Member_management.activated_member_list);
+
+// Member_management.login('waka', '0724');
+// console.log(Member_management.activated_member_list);
+Member_management.login('wak', '0724');
+console.log(Member_management.activated_member_list);
+
+Member_management.activated_member_list['wak'].basket.add_order({
+    'menu': '주문3',
+    'style': 'Dulexe', 
+    'foods_to_remove': [], 
+    'amount': 1, 
+    'additional_info': []
+})
+// pay test
+Member_management.activated_member_list['wak'].pay();
+*/
