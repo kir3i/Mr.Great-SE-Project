7ee26c70ba15bba109ec2356298757bdb60df84b
kir3i <kirei7081@gmail.com>
2020-11-27 09:54:41 +0900

    메뉴 이름 미출력 문제 해결

--- source/server/modules/Menu.mjs
+++ source/server/modules/Menu.mjs
@@ -1,37 +1,11 @@
 "use strict";
 
 export default class Menu {
-    #_name;
-    #_photo;
-    #_description;
-    #_price;
-    #_food_list;
-
     constructor(name, photo, description, price, food_list) {
-        this.#_name = name;
-        this.#_photo = photo;
-        this.#_description = description;
-        this.#_price = price;
-        this.#_food_list = food_list;
-    }
-
-    get name() {
-        return this.#_name;
-    }
-
-    get photo() {
-        return this.#_photo;
-    }
-    
-    get description() {
-        return this.#_description;
-    }
-
-    get price() {
-        return this.#_price;
-    }
-
-    get food_list() {
-        return this.#_food_list;
+        this.name = name;
+//        this.photo = photo;
+        this.description = description;
+        this.price = price;
+        this.food_list = food_list;
     }
 }
