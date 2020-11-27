62115142c30dd095f405b68c6a98672faf7c940d
kir3i <kirei7081@gmail.com>
2020-11-27 11:45:06 +0900

    Order의 가격 계산 시 세부항목이 0개인 경우 반영되게 수정

--- source/server/modules/Order.mjs
+++ source/server/modules/Order.mjs
@@ -42,6 +42,8 @@ export default class Order {
             if (this.food_amount_list[food] > 1) {
                 price += (this.menu.food_list[food])*(this.food_amount_list[food] - 1);
                 // console.log(this.menu.food_list[food], this.food_amount_list[food] - 1);
+            } else (this.food_amount_list[food] == 0) {
+                price -= (this.menu.food_list[food]);
             }
         }
         price *= this.amount;
